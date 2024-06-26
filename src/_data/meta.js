82a9b8d5f8145fa25115c5acef5bac923b495735
madrilene <hola@lenesaile.com>
2024-06-26 09:37:30 +0200

    add and integrate global data, og image

---
+++ src/_data/meta.js
@@ -0,0 +1,4 @@
+export const url = process.env.URL || 'http://localhost:8080';
+export const siteName = 'Eleventy minimal';
+export const siteDescription =
+  'Very minimal setup for Eleventy 3.0.  Built with the help of CUBE CSS, Utopia and Every Layout.';
